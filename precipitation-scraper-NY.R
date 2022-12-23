825b05f4351f0b58a5e5986a06ff833af8a5e969
Alexandra Harris <103076913+alexandramharris@users.noreply.github.com>
2022-12-23 15:31:18 -0500

    Update precipitation-scraper-NY.R

--- precipitation-scraper-NY.R
+++ precipitation-scraper-NY.R
@@ -74,13 +74,16 @@ scraper$Time <- sub("^0", "", scraper$Time)
 scraper$Time <- gsub("AM", "a.m.", scraper$Time)
 scraper$Time <- gsub("PM", "p.m.", scraper$Time)
 
+# Concatenate day and time
+scraper$Reported <- paste(scraper$`Day reported`, scraper$Time)
+
 # Filter for counties
 scraper <- scraper %>% 
   filter(!is.na(County) & (County == "Albany" | County == "Saratoga" | County == "Schenectady" | County == "Rensselaer" | County == "Greene" | County == "Ulster" | County == "Delaware" | County == "Sullivan" | County == "Columbia" | County == "Greene" | County == "Dutchess" | County == "Putnam" | County == "Orange" | County == "
 Washington" | County == "Warren" | County == "Schoharie" | County == "Montgomery" | County == "Fulton" | County == "Hamilton"))
 
 # Move inches to end
-scraper <- select(scraper, Date, Time, State, County, Location, `Location 2`, `Location 3`, Latitude, Longitude, Precipitation, Method, Measurement, `Day reported`, Inches, Unit)
+scraper <- select(scraper, Date, Time, State, County, Location, `Location 2`, `Location 3`, Latitude, Longitude, Precipitation, Method, Measurement, `Day reported`, Reported, Inches, Unit)
 
 # Trim numbers after location name
 scraper$Location <- sub("\\d.*", "", scraper$Location)

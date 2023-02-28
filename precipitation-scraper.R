554e71ab6c27e9cd62648fa900f2bb4e16ccd237
Vivien Ngo <vivngo@users.noreply.github.com>
2023-02-28 14:33:34 -0500

    Add regex for numbers at end of string

--- precipitation-scraper.R
+++ precipitation-scraper.R
@@ -295,8 +295,9 @@ for(i in 1:length(links)){
   # Move inches to end
   ct_scraper <- select(ct_scraper, Date, Time, State, County, Location, `Location 2`, `Location 3`, Latitude, Longitude, Precipitation, Method, Measurement, `Day reported`, Reported,`Daily date`, `Date reported`, Inches, Unit)
   
-  # Trim numbers after location name
-  ct_scraper$Location <- sub("^[0-9]+ [NESW]+ ", "", ct_scraper$Location)
+  # Trim numbers before & after location name
+  ct_scraper$Location <- sub("^[0-9.]+ [NESW]+ ", "", ct_scraper$Location)
+  ct_scraper$Location <- sub(" [0-9.]+ [NESW]+$", "", ct_scraper$Location)
   
   # Create daily dataset
   ct_snow_daily_temp <- ct_scraper %>% 
